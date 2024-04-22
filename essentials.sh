604c1eeacfc2e3793ac1426fbc258c30e941be9a
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2024-04-22 15:13:45 +0200

    radare2 from github

--- essentials.sh
+++ essentials.sh
@@ -2,4 +2,5 @@
 
 apt install -f bash unzip git zsh curl p7zip-full;
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/radareorg/radare2.git
  
