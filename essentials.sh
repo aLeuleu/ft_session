863453278f69db8b9f0f8ccbdf367b76608804ce
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2024-04-22 15:04:01 +0200

    blind patch : force install

--- essentials.sh
+++ essentials.sh
@@ -1,5 +1,5 @@
 !#/bin/sh
 
-apt install bash unzip git zsh curl 7z;
+apt install -f bash unzip git zsh curl 7z;
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
  
